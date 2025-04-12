c2f9943aa7c103571cdd77f3a72b523a8b953f7d
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-04-12 21:47:28 +0200

    header_update

--- wannabearockstar-fe/src/components/Header.jsx
+++ wannabearockstar-fe/src/components/Header.jsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React from "react";
 
-export default function Header() {
+const Header = () => {
   return (
-    <header className="header-section">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-3 col-md-3">
-            <div className="site-logo">
-              <h2>Musico</h2>
+    <header>
+      <div className="header-area">
+        <div id="sticky-header" className="main-header-area">
+          <div className="container-fluid">
+            <div className="header_bottom_border">
+              <div className="row align-items-center">
+                <div className="col-xl-3 col-lg-2">
+                  <div className="logo">
+                    <a href="/">
+                      <img src="img/logo.png" alt="Logo" />
+                    </a>
+                  </div>
+                </div>
+                <div className="col-xl-6 col-lg-7">
+                  <div className="main-menu d-none d-lg-block">
+                    <nav>
+                      <ul id="navigation">
+                        <li><a className="active" href="/">home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/track">Tracks</a></li>
+                        <li>
+                          <a href="#">Blog <i className="ti-angle-down"></i></a>
+                          <ul className="submenu">
+                            <li><a href="/blog">Blog</a></li>
+                            <li><a href="/single-blog">Single Blog</a></li>
+                          </ul>
+                        </li>
+                        <li>
+                          <a href="#">Pages <i className="ti-angle-down"></i></a>
+                          <ul className="submenu">
+                            <li><a href="/elements">Elements</a></li>
+                          </ul>
+                        </li>
+                        <li><a href="/contact">Contact</a></li>
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-lg-3 d-none d-lg-block">
+                  <div className="social_icon text-right">
+                    <ul>
+                      <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                      <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                      <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="mobile_menu d-block d-lg-none"></div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="col-lg-9 col-md-9">
-            <nav className="main-menu">
-              <ul>
-                <li className="active"><a href="#">Home</a></li>
-                <li><a href="#">Albums</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
-            </nav>
           </div>
         </div>
       </div>
     </header>
   );
-}
+};
+
+export default Header;
